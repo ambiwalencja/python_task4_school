d6d48a74ce595dc223110823c0b5fec35869657f
ambiwalencja <helena.a.sokolowska@gmail.com>
2021-05-12 20:09:39 +0200

    changed classes and added first loop for adding students

--- school.py
+++ school.py
@@ -1,43 +1,63 @@
 import sys
 
+ALLOWED_COMMANDS = ("student", "stop")
+list_of_students = []  # global list of dicts?
+
 
 class Group:  # school class
-    def __init__(self, tutor, students):
-        self.tutor = tutor
-        self.students = students  # list of tuples?
+    def __init__(self, t, s):
+        self.tutor = t
+        self.students = s  # list of ???
 
-    def add_student(self, new_student):  # new_student comes from input
-        self.student.append(new_student)
 
-    def print_students(self):
-        for first_name, last_name in self.students:
-            print(first_name, last_name)
+#   def add_student(self, new_student):  # new_student comes from input
+#       self.students.append(new_student)
+
+#   def print_students(self):
+#      for first_name, last_name in self.students:
+#          print(first_name, last_name)
 
 
 class Student:
-    def __init__(self, group, first_name, last_name, subjects):
-        self.group = group
-        self.first_name = first_name
-        self.last_name = last_name
-        self.name = (first_name, last_name)
-        self.subjects = subjects
+    def __init__(self, nm, grp, sbjct):
+        self.group = grp
+        # self.first_name = first_name
+        # self.last_name = last_name
+        self.name = nm
+        self.subjects = sbjct
+
+    def data(self):
+        student_data = {self.name: {"group": self.group, "subjects": self.subjects}}
+        return student_data
 
 
 class Tutor:
-    def __init__(self, groups, first_name, last_name,):
+    def __init__(self, groups, nm):
         self.groups = groups
-        self.first_name = first_name
-        self.last_name = last_name
-        self.name = (first_name, last_name)
+        self.name = nm
 
 
 class Teacher:
-    def __init__(self, subject, first_name, last_name,):
-        self.subject = subject
-        self.first_name = first_name
-        self.last_name = last_name
-        self.name = (first_name, last_name)
+    def __init__(self, sbjct, nm):
+        self.subject = sbjct
+        self.name = nm
 
 
-#while True:
-print("test")
+while True:
+    user = input("Type a user: ")
+    if user not in ALLOWED_COMMANDS:
+        print(f"allowed commands are: {ALLOWED_COMMANDS}")
+        continue
+    if user == "stop":
+        break
+    if user == "student":
+        name = input("Type name: ")
+        group = input("Type group: ")
+        subjects = []
+        subject = input("Type subject: ")
+        while subject:
+            subjects.append(subject)
+            subject = input("Type next subject: ")
+        student_object = Student(name, group, subjects)
+        list_of_students.append(student_object.data())
+        print(list_of_students)
